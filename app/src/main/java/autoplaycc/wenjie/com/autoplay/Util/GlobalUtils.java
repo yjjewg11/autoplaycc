c5677ae9d5a7e15565eecf27914d398f9c90049f
tanghongbin <18780475970@163.com>
2016-03-04 22:52:08 +0800

    1.初次提交。

---
+++ app/src/main/java/autoplaycc/wenjie/com/autoplay/Util/GlobalUtils.java
@@ -0,0 +1,9 @@
+package autoplaycc.wenjie.com.autoplay.Util;
+
+/**
+ * Created by tangt on 2016/3/4.
+ */
+public interface GlobalUtils {
+    String httpPath = "http://www.wenjienet.com/px-cc/index.html";
+    String testPath =       "http://www.baidu.com";
+}
